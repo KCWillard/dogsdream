380f0bf2f6c3b4f486c88ca350f8492fdc22c894
Malgorzata Sinha <sklarekm@oregonstate.edu>
2020-07-15 15:12:54 -0700

    added register

--- app.py
+++ app.py
@@ -74,6 +74,19 @@ def login():
     return render_template('login.html', error=error)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            if request.form['profiles'] == 'sitter':
+            else:
+                return redirect(url_for('owner'))
+    return render_template('register.html', error=error)
+
+
 @app.route('/owner/')
 def owner():
     return render_template('owner/owner.html')
